8fdbb4cea5115364fae3fcfa3c1dfcc457574c0f
E1Code <39209383+E1Code@users.noreply.github.com>
2018-06-09 17:20:00 -0700

    Update clientBinaryManager.js

--- modules/clientBinaryManager.js
+++ modules/clientBinaryManager.js
@@ -13,7 +13,7 @@ const log = require('./utils/logger').create('ClientBinaryManager');
 
 // should be       'https://raw.githubusercontent.com/ethereum/mist/master/clientBinaries.json'
 const BINARY_URL =
-  'https://raw.githubusercontent.com/ethereum/mist/master/clientBinaries.json';
+  'https://github.com/Ether1Project/Ether-1-Desktop-Wallet/blob/Ether-1-0.0.1/clientBinaries.json';
 
 const ALLOWED_DOWNLOAD_URLS_REGEX = /^https:\/\/(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)?ethereum\.org\/|gethstore\.blob\.core\.windows\.net\/|bintray\.com\/artifact\/download\/karalabe\/ethereum\/)(?:.+)/; // eslint-disable-line max-len
 
