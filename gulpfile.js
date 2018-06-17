005f37256a907a7da654f7b98530a16dbf1c8d73
suijiaza <40181225+suijiaza@users.noreply.github.com>
2018-06-17 13:26:32 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ const options = minimist(args, {
   boolean: _.flatten(['wallet', platforms]),
   default: {
     wallet: false,
-    walletSource: 'Ether-1',
+    walletSource: 'master',
     test: 'basic',
     skipTasks: ''
   }
